675f6cfbec5e0161cd775b8f2fac39aed445431c
xescr <114061880+xescr@users.noreply.github.com>
2023-01-21 13:44:55 +0800

    Added Book Constructor

---
+++ js/scripts.ts
@@ -0,0 +1,6 @@
+function Book(name: string, author: string, pageCount: number, status: string) {
+    this.name = name
+    this.author = author
+    this.pagecount = pageCount
+    this.status = status
+}
