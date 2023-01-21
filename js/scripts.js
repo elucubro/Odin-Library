3dda23877af1edb4de62e490678a9f72f361d714
xescr <114061880+xescr@users.noreply.github.com>
2023-01-21 15:48:04 +0800

    began working on card system however only one object in array myLibrary is being recognized

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,36 @@
+"use strict";
+let myLibrary = [];
 function Book(bookName, author, pageCount, bookStatus) {
     this.bookName = bookName;
     this.author = author;
     this.pageCount = pageCount;
     this.bookStatus = bookStatus;
-    function info() {
-        return ({ bookName: bookName } +
-            " " +
-            { author: author } +
-            " " +
-            { pageCount: pageCount } +
-            " " +
-            { bookStatus: bookStatus });
+    this.info = function () {
+        return (bookName + ", " + author + ", " + pageCount.toString() + ", " + bookStatus);
+    };
+}
+function addBookToLibrary(titleInput, authorInput, pageCount, statusInput) {
+    let page = Number(pageCount);
+    let newBook = new Book(titleInput, authorInput, page, statusInput);
+    myLibrary.push(newBook);
+}
+function addCards() {
+    for (let item of myLibrary) {
+        const libraryContainer = document.createElement("div");
+        libraryContainer.classList.add("libraryContainer");
+        const itemCard = document.createElement("div");
+        itemCard.classList.add("itemCard");
+        // I want this div to be a column directed flexbox
+        const nameAuthor = `${item.bookName} ${item.author}`;
+        const header = document.createElement("h1");
+        header.textContent = nameAuthor;
+        header.classList.add("cardHeader");
+        // Appending elements to various containers
+        itemCard.appendChild(header);
+        libraryContainer.appendChild(itemCard);
+        document.body.appendChild(libraryContainer);
     }
 }
-var theHobbit = new Book("The Hobbit", "JRR Tolkien", 134, "Read");
+const theHobbit = new Book("The Hobbit", "JRR Tolkien", 134, "True");
 console.log(theHobbit.info());
+myLibrary.push(theHobbit);
--- js/scripts.ts
+++ js/scripts.ts
@@ -1,3 +1,4 @@
+let myLibrary: any[] = [];
 function Book(
   this: any,
   bookName: string,
@@ -9,18 +10,43 @@ function Book(
   this.author = author;
   this.pageCount = pageCount;
   this.bookStatus = bookStatus;
-  function info() {
+  this.info = function () {
     return (
-      { bookName } +
-      " " +
-      { author } +
-      " " +
-      { pageCount } +
-      " " +
-      { bookStatus }
+      bookName + ", " + author + ", " + pageCount.toString() + ", " + bookStatus
     );
+  };
+}
+
+function addBookToLibrary(
+  titleInput: string,
+  authorInput: string,
+  pageCount: string,
+  statusInput: string
+) {
+  let page = Number(pageCount);
+  let newBook = new (Book as any)(titleInput, authorInput, page, statusInput);
+  myLibrary.push(newBook);
+}
+
+function addCards() {
+  for (let item of myLibrary) {
+    const libraryContainer = document.createElement("div");
+    libraryContainer.classList.add("libraryContainer");
+    const itemCard = document.createElement("div");
+    itemCard.classList.add("itemCard");
+    // I want this div to be a column directed flexbox
+    const nameAuthor = `${item.bookName} ${item.author}`;
+    const header = document.createElement("h1");
+    header.textContent = nameAuthor;
+    header.classList.add("cardHeader");
+
+    // Appending elements to various containers
+    itemCard.appendChild(header);
+    libraryContainer.appendChild(itemCard);
+    document.body.appendChild(libraryContainer);
   }
 }
 
-const theHobbit = new (Book as any)("The Hobbit", "JRR Tolkien", 134, "Read");
+const theHobbit = new (Book as any)("The Hobbit", "JRR Tolkien", 134, "True");
 console.log(theHobbit.info());
+myLibrary.push(theHobbit);
